7e85b3bcccaef694de59b5ed24137a7e4c3c5a9b
Dara Shorten <dara.shorten@mycit.ie>
2017-11-08 17:35:29 -0800

    Updated profile site

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var image_min = require('gulp-imagemin');
+var image_resize = require('gulp-image-resize');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+let cleanCSS = require('gulp-clean-css');
+var del = require('del');
+var parallel = require('concurrent-transform');
+var debug = require('gulp-debug');
+
+var paths = {
+	images: './img/portfolio/*.{jpg,png}',
+	style: './css/agency.css'
+}
+
+gulp.task('clean', () =>{
+	return del(['build']);
+});
+
+gulp.task('minify-css', ['clean'], () => {
+  return gulp.src(paths.style)
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('images', ['clean'],() =>{
+	return gulp.src('./img/portfolio/*.{jpg,png,JPG,gif}')
+		.pipe(debug())
+		.pipe(parallel(
+			image_resize({
+				width: 150,
+				height: 130,
+				crop: false
+			}))
+		)
+		.pipe(image_min({optimizationLevel: 5}))
+		.pipe(gulp.dest('build/img'));
+});
+
+gulp.task('default', ['minify-css', 'images']);
